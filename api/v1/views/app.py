552689b6d4ce225a108f50298da3858130cb7d72
francotaind <francotaind00@gmail.com>
2024-05-02 08:54:53 +0300

    app_views

---
+++ api/v1/views/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+setup flask app
+
+"""
+import os
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+app.register_blueprint(app_views)
+
+CORS(app, resources={"/*":{"origins":"0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Tears down storage context"""
+    storage.close()
+
+@app.errorhandler(404)
+def error_not_found(error):
+    """Handles 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == '__main__'
+    host = os.getenv('HBNB_API_HOST','0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
+    
